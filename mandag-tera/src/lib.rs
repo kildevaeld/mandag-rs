f9eb9501c1b7e2e302b198a5bde8e2742aa5b33f
Rasmus Kildevæld <raki@totalkredit.dk>
2022-12-13 15:20:12 +0100

    create tera extension

---
+++ mandag-tera/src/lib.rs
@@ -0,0 +1,68 @@
+use std::{future::Future, process::Output, sync::Arc};
+
+use mandag_core::{
+    async_trait,
+    dale::Service,
+    http::{reply, Body, Error, Reply},
+    prelude::*,
+    Extension, ExtensionCtx, Request,
+};
+use relative_path::RelativePathBuf;
+
+pub use tera::Context;
+
+#[derive(Debug, Clone)]
+pub struct Tera(Arc<tera::Tera>);
+
+impl std::ops::Deref for Tera {
+    type Target = tera::Tera;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_ref()
+    }
+}
+
+pub struct TeraExt;
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct TeraConfig {
+    path: RelativePathBuf,
+}
+
+#[async_trait]
+impl<C> Extension<C> for TeraExt
+where
+    C: ExtensionCtx,
+{
+    async fn build(&self, ctx: &mut C) -> Result<(), Error> {
+        let cfg: TeraConfig = ctx.config().try_get("templates").expect("tera config");
+
+        let path = cfg.path.join("**/*").to_string();
+
+        let tera = tera::Tera::new(&path).map_err(Error::new)?;
+
+        ctx.register(Tera(Arc::new(tera)));
+
+        Ok(())
+    }
+}
+
+impl Tera {
+    pub fn template(
+        path: &str,
+    ) -> impl Service<Request, Output = reply::Html<String>, Future = impl Future + Send> {
+        let path = path.to_string();
+        move |req: Request| {
+            let path = path.clone();
+            async move {
+                let tera: Tera = req.app().store().get().expect("fetching Tera from store");
+
+                let ctx = Context::default();
+
+                let template = tera.render(&path, &ctx).map_err(Error::new).unwrap();
+
+                reply::html(template)
+            }
+        }
+    }
+}
